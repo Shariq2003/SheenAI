a3b31684d251c693b3349c7bdb9f576e1998feae
Shariq <shariqsd2003@gmail.com>
2026-08-30 19:29:32 +0530

    CORS Fixes

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -43,7 +43,7 @@ class Settings(BaseSettings):
 
     # --- CORS ---
     cors_origins: str = Field(
-        default="http://localhost:5173,http://127.0.0.1:5173, https://sheenai.onrender.com",
+        default="http://localhost:5173,http://127.0.0.1:5173,https://sheenai.vercel.app",
         alias="CORS_ORIGINS",
     )
 
